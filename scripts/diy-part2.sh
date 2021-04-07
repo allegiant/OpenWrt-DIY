e3a45b493db25f3d76acc4c1976cf4d363a2655d
allegiant <allegiant.xu@hotmail.com>
2021-04-07 09:14:35 +0800

    Update diy-part2.sh

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -25,6 +25,9 @@ pushd package/community
 # Add Lienol's Packages
 git clone --depth=1 https://github.com/Lienol/openwrt-package
 
+# Add luci-app-dnsfilter 
+git clone --depth=1 https://github.com/garypang13/luci-app-dnsfilter 
+
 # Add luci-app-passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
 
